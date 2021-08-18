d14d375fd010176f4afc69b9daa5844506cfe76c
Lamed(라메드) <53391835+Lamed-D@users.noreply.github.com>
2021-08-18 21:13:26 +0900

    Z

---
+++ Sample/소스.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    HANDLE File = CreateFileA(
+        "abex.l2c",
+        GENERIC_READ,
+        0,
+        0,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        0);
+    char buf[256];
+    DWORD dwRead;
+    ReadFile(File, buf, sizeof(buf), &dwRead, NULL);
+    printf("%s", buf);
+}
